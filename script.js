e7c58827107ab3c96860b6160ca0aca172ea1e59
AhmedAfni <ahmedafni86@gmail.com>
2024-07-24 22:34:37 +0530

    small changes on code

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const txtInput = document.querySelector(".inputs input"); 
+checkBtn = document.querySelector(".inputs button")
 
 txtInput.addEventListener("keyup", () => {
-    console.log(txtInput.value)
+    //removing spaces & all special characters from entered value
+    let filterInput = txtInput.value.replace(/[^A-Z0-9]/ig, "")
+    if(filterInput) {
+        return checkBtn.classList.add("active");
+    }
+    checkBtn.classList.remove("active");
 })
