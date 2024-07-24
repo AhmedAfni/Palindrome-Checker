fdfc75b824160fc560201bf155271b6875a74350
AhmedAfni <ahmedafni86@gmail.com>
2024-07-24 23:26:34 +0530

    finished the all the part :)

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 const txtInput = document.querySelector(".inputs input"); 
 const checkBtn = document.querySelector(".inputs button");
+const infoText = document.querySelector(".info-text"); 
 let filterInput;
 
 checkBtn.addEventListener("click", () => {
-    console.log(filterInput);
+    // Splitting user input characters, reversing them,
+    // and joining them into a single word
+    let reverseInput = filterInput.split("").reverse().join("");
+    infoText.style.display = "block";
+    console.log(filterInput, reverseInput);
+    if(filterInput !== reverseInput) {
+        return infoText.innerHTML = `No, <span>'${txtInput.value}'</span> isn't a palindrome`;
+    }
+    infoText.innerHTML = `Yes, <span>'${txtInput.value}'</span> is a palindrome`;
 });
 
 txtInput.addEventListener("keyup", () => {
-    // Removing spaces & all special characters from entered value
-    filterInput = txtInput.value.replace(/[^A-Z0-9]/ig, "");
+    // Removing spaces & all special characters from the entered value
+    filterInput = txtInput.value.toLowerCase().replace(/[^a-z0-9]/gi, "");
     if(filterInput) {
         return checkBtn.classList.add("active");
     }
+    infoText.style.display = "none";
     checkBtn.classList.remove("active");
 });
